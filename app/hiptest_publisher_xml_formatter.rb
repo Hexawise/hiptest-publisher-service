ef7786f26365dce87530c37b1203a2ad6ed38b41
Eric Musgrove <tenpaiyomi@users.noreply.github.com>
2019-03-29 18:32:38 -0400

    Lots of changes to make the publisher work better with scenario blocks that happen to have parameters (generally for parsing to create action steps that can work across multiple scenario / scenario outline blocks), and cleaning up the logic a bit

--- app/hiptest_publisher_xml_formatter.rb
+++ app/hiptest_publisher_xml_formatter.rb
@@ -1,7 +1,5 @@
 class HiptestPublisherXMLFormatter
   def self.format(script)
-    script_actionwords = get_actionwords(script.scenarios)
-
     xml = Builder::XmlMarkup.new(indent: 2)
     xml.instruct! :xml, encoding: 'UTF-8'
     xml.project format: '0.1' do |project|
@@ -21,6 +19,7 @@ class HiptestPublisherXMLFormatter
           end
         end
       end
+
       project.scenarios do |scenarios|
         script.scenarios.each do |script_scenario|
           scenarios.scenario do |scenario|
@@ -28,12 +27,12 @@ class HiptestPublisherXMLFormatter
 
             setup_scenario_tags(scenario, script_scenario)
             setup_scenario_datatable_and_parameters(scenario, script_scenario)
-            setup_scenario_steps(scenario, script_scenario, script_actionwords)
+            setup_scenario_steps(scenario, script_scenario)
           end
         end
       end
 
-      setup_actionwords(project, script_actionwords)
+      setup_actionwords(project, script.scenarios)
     end
 
     xml.target!
@@ -60,7 +59,8 @@ class HiptestPublisherXMLFormatter
   #   <actionword>
   # </actionwords>
   #
-  def self.setup_actionwords(project, script_actionwords)
+  def self.setup_actionwords(project, script_scenarios)
+    script_actionwords = get_actionwords(script_scenarios)
     project.actionwords do |actionwords|
       script_actionwords.each do |script_actionword, actionword_parameters|
         actionwords.actionword do |actionword|
@@ -167,29 +167,28 @@ class HiptestPublisherXMLFormatter
   #       ...
   #     </arguments>
   #
-  def self.setup_scenario_steps(scenario, script_scenario, script_actionwords)
+  def self.setup_scenario_steps(scenario, script_scenario)
     scenario.steps do |steps|
       script_scenario[:steps].each do |scenario_step|
         steps.call do |call|
-          scenario_parameters = parameters_for_step_text(scenario_step[:text])
-          step_action = format_step_text_for_parameters(scenario_step[:text])
-          step_parameters = script_actionwords[step_action]
+          scenario_parameters = parameters_for_scenario_step_text(script_scenario[:type], scenario_step[:text])
+          step_parameters = parameters_for_step_text(scenario_step[:text])
 
-          call.actionword step_action
+          call.actionword format_step_text_for_parameters(script_scenario[:type], scenario_step[:text], scenario_parameters)
           call.annotation scenario_step[:keyword].strip
 
           if step_parameters.any?
             call.arguments do |arguments|
               step_parameters.each_with_index do |step_parameter, index|
                 arguments.argument do |argument|
-                  argument.name step_parameter # scenario_parameter
+                  argument.name script_scenario[:type] == :ScenarioOutline ? step_parameter : scenario_parameters[index]
                   argument.value do |value|
-                    if script_scenario[:type] == :Scenario
-                      value.template do |template|
-                        template.stringliteral scenario_parameters[index]
-                      end
+                    if script_scenario[:type] == :ScenarioOutline
+                      value.var step_parameter&.tr('<>', '')&.strip&.gsub("\n", '\n')
                     else
-                      value.var step_parameter.tr('<>', '')
+                      value.template do |template|
+                        template.stringliteral step_parameter.tr('<>', '')&.strip&.gsub("\n", '\n')
+                      end
                     end
                   end
                 end
@@ -214,7 +213,7 @@ class HiptestPublisherXMLFormatter
   # </tags>
   #
   def self.setup_scenario_tags(scenario, script_scenario)
-    return if script_scenario[:tags].nil?
+    return if script_scenario[:tags].empty?
 
     scenario.tags do |tags|
       script_scenario[:tags].each do |script_tag|
@@ -235,29 +234,33 @@ class HiptestPublisherXMLFormatter
   # generate actionwords for.
   def self.get_actionwords(scenarios)
     actionwords = {}
-    parameter_index = 0
     scenarios.each do |scenario|
       scenario[:steps].each do |step|
-        step_parameters = parameters_for_step_text(step[:text]).map do |parameter|
-          if scenario[:type] == :ScenarioOutline
-            parameter
-          else
-            parameter_index += 1
-            "p#{parameter_index}"
-          end
-        end
+        step_parameter_values = parameters_for_scenario_step_text(scenario[:type], step[:text])
+        step_parameter_names = parameters_for_step_text(step[:text])
 
-        actionwords[format_step_text_for_parameters(step[:text])] ||= step_parameters
+        actionwords[format_step_text_for_parameters(scenario[:type], step[:text], step_parameter_values)] ||= []
+        actionwords[format_step_text_for_parameters(scenario[:type], step[:text], step_parameter_values)] += step_parameter_values
       end
     end
 
+    actionwords = actionwords.each { |k,v| actionwords[k] = v.uniq }
     actionwords
   end
 
   ##
   # Accepts the scenario step text for our current step, then
   # grabs the parameters for it, and replaces each parameter such
-  # that our step text will match a given actionword.
+  # that our step text will match a given actionword, depending on
+  # if it is a scenario or scenario outline
+  #
+  # scenario:
+  #
+  # the account balance is <account_balance>
+  # becomes
+  # the account balance is "p1"
+  #
+  # scenario outline:
   #
   # the account balance is <account_balance>
   # becomes
@@ -273,8 +276,12 @@ class HiptestPublisherXMLFormatter
   # becomes
   # the account balance is "<account_balance>"
   #
-  def self.format_step_text_for_parameters(scenario_step_text)
-    parameters_for_step_text(scenario_step_text).reduce(scenario_step_text) { |step_text, parameter| step_text.gsub(parameter, "\"#{parameter}\"").gsub("\"\"#{parameter}\"\"", "\"#{parameter}\"") }.strip
+  def self.format_step_text_for_parameters(scenario_type, scenario_step_text, step_parameters)
+    if scenario_type == :ScenarioOutline
+      parameters_for_step_text(scenario_step_text).reduce(scenario_step_text) { |step_text, parameter| step_text.gsub(parameter, "\"#{parameter}\"").gsub("\"\"#{parameter}\"\"", "\"#{parameter}\"") }.strip
+    else
+      parameters_for_step_text(scenario_step_text).each_with_index.reduce(scenario_step_text) { |step_text, (parameter, index)| step_text.gsub(parameter, "\"#{step_parameters[index]}\"").gsub("\"\"#{step_parameters[index]}\"\"", "\"#{step_parameters[index]}\"") }.strip
+    end
   end
 
   ##
@@ -300,5 +307,22 @@ class HiptestPublisherXMLFormatter
   #
   def self.parameters_for_step_text(scenario_step_text)
     scenario_step_text.scan(/(<[^>]+>)/).flatten.compact
+  end
+
+  ##
+  # Accepts the scenario type and scenario text, then calls the
+  # #parameters_for_step_text to get the parameters that exist on a given
+  # line of text for a step.  From there, if this is a scenario outline, we
+  # return the parameter value itself, in the form of <parameter_value>.
+  # If it is a scenario, then we return an indexed parameter, in the form
+  # of <p#>, where # is the current index value.
+  def self.parameters_for_scenario_step_text(scenario_type, scenario_step_text)
+    parameters_for_step_text(scenario_step_text).each_with_index.map do |parameter, index|
+      if scenario_type == :ScenarioOutline
+        parameter
+      else
+        "<p#{index+1}>"
+      end
+    end
   end
 end
